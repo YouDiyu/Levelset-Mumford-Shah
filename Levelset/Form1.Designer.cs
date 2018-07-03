d9bbb8a9c5441132b935df7dc4e2c30d43e979b7
YouDiyu <YouDiyu@users.noreply.github.com>
2018-07-03 02:10:27 +0200

    Initial commit

---
+++ Levelset/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Levelset
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(449, 137);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(290, 388);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(14, 137);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(417, 387);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Initialisierung";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(101, 25);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Radiu";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(325, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 32);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(95, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(101, 25);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "IterN";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(227, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "nu";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(227, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "mu";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(384, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Lamda";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(384, 109);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "TimeS";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(265, 63);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(101, 25);
+            this.textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(265, 106);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(101, 25);
+            this.textBox5.TabIndex = 16;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(449, 63);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(101, 25);
+            this.textBox6.TabIndex = 17;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(449, 106);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(101, 25);
+            this.textBox7.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 537);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ Levelset/Form1.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Levelset
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image tmpImage;
+        Bitmap oriimage;//originale Image
+        Bitmap oriimage0;
+        Bitmap oriimage1;
+        Size tmpsiz;
+        string Filename;
+        bool mousedown1 = false;
+        int x;
+        Point pt;
+        double[,] Mphi;
+        Graphics graf = null;
+        double[,] MDirac;
+        double[,] MHeaviside;
+        double[,] MCurv;
+        double[,] MPenalize;
+        double[,] dx, dy, ddx, ddy;
+        double[,] Kontur;
+        double c1, c2;
+        int iterNum;
+        double nu, mu, lamda, timestep;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog opd = new OpenFileDialog();
+            opd.DefaultExt = ".bmp";
+            opd.Filter = "Image Files(*.bmp,*.jpg,*.png,*.TIF)|*.bmp;*.jpg;*.png;*.TIF||";
+            if (DialogResult.OK != opd.ShowDialog(this))
+            {
+                return;
+            }
+            Filename = opd.FileName;
+            textBox1.AppendText(opd.FileName);
+            loadimage();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            x = Convert.ToInt32(textBox2.Text);
+            if (mousedown1 == false)
+            {
+                mousedown1 = true;
+                button2.Enabled = false;
+            }
+            else
+            {
+                return;
+            }
+            textBox1.AppendText("Koordinate: " + Convert.ToDouble(pt.X) + "," + Convert.ToDouble(pt.Y) + "\n");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            iterNum = Convert.ToInt16(textBox3.Text);
+            nu = Convert.ToDouble(textBox4.Text);
+            mu = Convert.ToDouble(textBox5.Text);
+            lamda = Convert.ToDouble(textBox6.Text);
+            timestep = Convert.ToDouble(textBox7.Text);
+            for (int itr = 0; itr < iterNum; itr++)
+            {
+                Heaviside();
+                Dirac();
+                curv();
+                BrGray();
+                //Sobel();
+                for (int i = 0; i < oriimage1.Width; i++)
+                {
+                    for (int j = 0; j < oriimage1.Height; j++)
+                    {
+                        Mphi[i, j] = Mphi[i, j] + timestep * MDirac[i, j] * (nu * MCurv[i, j] - Math.Pow(oriimage1.GetPixel(i, j).R - c1, 2) + lamda * Math.Pow(oriimage1.GetPixel(i, j).R - c2, 2));
+                       
+                        //double lengthtr = nu * MDirac[i, j] * MCurv[i, j];
+                        //double penalizetr = mu * (MPenalize[i, j] - MCurv[i, j]);
+                        //double areatr = MDirac[i, j] *  (Math.Pow(oriimage1.GetPixel(i, j).R - c1, 2) - lamda * Math.Pow(oriimage1.GetPixel(i, j).R - c2, 2));
+                        //Mphi[i, j] = Mphi[i, j] + timestep * MDirac[i, j]*(lengthtr + areatr);
+                    }
+                }
+                plot1();
+                
+            }
+            
+        }
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (mousedown1 == true)
+            {
+                pt = new Point(e.Location.X, e.Location.Y);
+                Initialisierung();
+                mousedown1 = false;
+            }
+        }
+        public void loadimage()
+        {
+            tmpImage = Image.FromFile(Filename);
+            Size pansiz = panel1.ClientSize;
+            tmpsiz = tmpImage.Size;
+            if (tmpsiz.Width > pansiz.Width || tmpsiz.Height > pansiz.Height)
+            {
+                double rImage = tmpsiz.Width * 1.0 / tmpsiz.Height;
+                double rWnd = pansiz.Width * 1.0 / pansiz.Height;
+                if (rImage < rWnd) // image more high
+                {
+
+                    tmpsiz.Height = pansiz.Height;
+                    tmpsiz.Width = (int)(tmpsiz.Height * rImage);
+                }
+                else //image is more wide
+                {
+
+                    tmpsiz.Width = pansiz.Width;
+                    tmpsiz.Height = (int)(pansiz.Width / rImage);
+                }
+            }
+            panel1.Size = tmpsiz;
+            oriimage = new Bitmap(tmpImage, tmpsiz);
+            Color pixcolor = Color.FromArgb(0);
+            double pixval = 0;
+            for (int i = 0; i < oriimage.Height; i++)
+            {
+                for (int j = 0; j < oriimage.Width; j++)
+                {
+                    pixcolor = oriimage.GetPixel(j, i);
+                    pixval = pixcolor.R * 0.3 + pixcolor.G * 0.59 + pixcolor.B * 0.11;
+                    oriimage.SetPixel(j, i, Color.FromArgb(Convert.ToInt32(pixval), Convert.ToInt32(pixval), Convert.ToInt32(pixval)));
+                }
+            }
+            oriimage1 = oriimage;
+            //Grauss();//useless
+            panel1.BackgroundImage = oriimage1;
+            textBox1.AppendText("Image load" + "\n");
+            panel1.Refresh();
+            oriimage0 = new Bitmap(oriimage1, tmpsiz);
+            //oriimage=>grau;oriimage1=>nach gaussian;oriimage0=>copy von oriimage1
+        }
+        public void Initialisierung()
+        {
+            graf = Graphics.FromImage(oriimage0);
+            Pen pen = new Pen(Color.Red);
+            graf.DrawEllipse(pen, pt.X - x, pt.Y - x, 2 * x, 2 * x);
+            graf.Save();
+            panel1.Refresh();
+            panel1.BackgroundImage = oriimage0;
+            panel1.Refresh();
+            button2.Enabled = true;
+            Mphi = new double[oriimage1.Width, oriimage1.Height];
+            for (int i = 0; i < oriimage1.Width; i++)
+            {
+                for (int j = 0; j < oriimage1.Height; j++)
+                {
+                    double c;
+                    c = Math.Sqrt((Math.Pow(i - pt.X, 2) + Math.Pow(j - pt.Y, 2)));
+                    if (c >= x )
+                    {
+                        Mphi[i, j] = x-c;
+                    }
+                    else
+                    {
+                        Mphi[i, j] = x-c;
+                    }
+                }
+            }
+        }
+        public void Dirac()
+        {
+            MDirac = new double[oriimage1.Width, oriimage1.Height];
+            dx = new double[oriimage1.Width, oriimage1.Height];
+            dy = new double[oriimage1.Width, oriimage1.Height];
+            for (int i = 0; i < oriimage1.Width; i++)
+            {
+                for (int j = 0; j < oriimage1.Height; j++)
+                {
+                    if (i == 0 || j == 0 || i == (oriimage1.Width - 1) || j == (oriimage1.Height - 1))
+                    {
+                        dx[i, j] = 0;
+                        dy[i, j] = 0;
+                    }
+                    else
+                    {
+                        dx[i, j] = 0.5 * (MHeaviside[i + 1, j] - MHeaviside[i, j]) / Math.Sqrt(Math.Pow(MHeaviside[i + 1, j] - MHeaviside[i, j], 2) + Math.Pow((MHeaviside[i, j + 1] - MHeaviside[i, j - 1]) / 2, 2) + 0.0000000001) + 0.5 * (MHeaviside[i, j] - MHeaviside[i - 1, j]) / Math.Sqrt(Math.Pow(MHeaviside[i, j] - MHeaviside[i - 1, j], 2) + Math.Pow((MHeaviside[i, j + 1] - MHeaviside[i, j - 1]) / 2, 2) + 0.0000000001);
+                        dy[i, j] = 0.5 * (MHeaviside[i, j + 1] - MHeaviside[i, j]) / Math.Sqrt(Math.Pow((MHeaviside[i + 1, j] - MHeaviside[i - 1, j]) / 2, 2) + Math.Pow(MHeaviside[i, j + 1] - MHeaviside[i, j], 2) + 0.0000000001) + 0.5 * (MHeaviside[i, j] - MHeaviside[i, j - 1]) / Math.Sqrt(Math.Pow((MHeaviside[i + 1, j] - MHeaviside[i - 1, j]) / 2, 2) + Math.Pow(MHeaviside[i, j] - MHeaviside[i, j - 1], 2) + 0.0000000001);
+                    }
+                    if (Math.Sqrt(Math.Pow(dx[i, j], 2) + Math.Pow(dy[i, j], 2)) != 0)
+                    {
+                        MDirac[i, j] = 1;
+                    }
+                    else
+                    {
+                        MDirac[i, j] = 0;
+                    }
+                }
+            }
+        }
+        public void Heaviside()
+        {
+            MHeaviside = new double[oriimage1.Width, oriimage1.Height];
+            for (int i = 0; i < oriimage1.Width; i++)
+            {
+                for (int j = 0; j < oriimage1.Height; j++)
+                {
+                    if (Mphi[i, j] >= 0)
+                    {
+                        MHeaviside[i, j] = 1;
+                    }
+                    else
+                    {
+                        MHeaviside[i, j] = 0;
+                    }
+
+                      
+                }
+            }
+        }
+        public void BrGray()
+        {
+            double sumfgval = 0;
+            double sumbkval = 0;
+            int fgn = 0;
+            int bkn = 0;
+            for (int i = 1; i < oriimage1.Width; i++)
+            {
+                for (int j = 1; j < oriimage1.Height; j++)
+                {
+                    if (Mphi[i, j] == 1)
+                    {
+                        sumfgval = sumfgval + oriimage1.GetPixel(i, j).R;
+                        fgn++;
+                        
+
+                    }
+                    else
+                    {
+                        sumbkval = sumbkval + oriimage1.GetPixel(i, j).R;
+                        bkn++;
+                        
+                    }
+                }
+            }
+            c1 = sumfgval / fgn;
+            c2 = sumbkval / bkn;
+            
+        }
+        public void curv()
+        {
+            MCurv = new double[oriimage1.Width, oriimage1.Height];
+            dx = new double[oriimage1.Width, oriimage1.Height];
+            dy = new double[oriimage1.Width, oriimage1.Height];
+            ddx = new double[oriimage1.Width, oriimage1.Height];
+            ddy = new double[oriimage1.Width, oriimage1.Height];
+            for (int i = 0; i < oriimage1.Width; i++)
+            {
+                for (int j = 0; j < oriimage1.Height; j++)
+                {
+                    if (i == 0 || j == 0 || i == (oriimage1.Width - 1) || j == (oriimage1.Height - 1))
+                    {
+                        dx[i, j] = 0;
+                        dy[i, j] = 0;
+                    }
+                    else
+                    {
+                        dx[i, j] = (Mphi[i + 1, j] - Mphi[i, j]) / Math.Sqrt(Math.Pow(Mphi[i + 1, j] - Mphi[i, j], 2) + Math.Pow((Mphi[i, j + 1] - Mphi[i, j - 1]) / 2, 2) + 0.0000000001);
+                        dy[i, j] = (Mphi[i, j + 1] - Mphi[i, j]) / Math.Sqrt(Math.Pow((Mphi[i + 1, j] - Mphi[i - 1, j]) / 2, 2) + Math.Pow(Mphi[i, j + 1] - Mphi[i, j], 2) + 0.0000000001);
+                    }
+                }
+            }
+            //ddx,ddy
+            for (int i = 0; i < oriimage1.Width; i++)
+            {
+                for (int j = 0; j < oriimage1.Height; j++)
+                {
+                    if (i == 0 || j == 0 || i == (oriimage1.Width - 1) || j == (oriimage1.Height - 1))
+                    {
+                        dx[i, j] = 0;
+                        dy[i, j] = 0;
+                    }
+                    else
+                    {
+                        ddx[i, j] = dx[i, j] - dx[i - 1, j];
+                        ddy[i, j] = dy[i, j] - dy[i, j - 1];
+                        MCurv[i, j] = ddx[i, j] + ddy[i, j];
+                    }
+                }
+            }
+        }
+        public void Sobel()
+        {
+            /* 0.5 1 0.5
+             *  1 -6 1
+             * 0.5 1 0.5
+             */
+            MPenalize = new double[oriimage1.Width, oriimage1.Height];
+            for (int i = 0; i < oriimage1.Width; i++)
+            {
+                for (int j = 0; j < oriimage1.Height; j++)
+                {
+                    if (i == 0 || j == 0 || i == (oriimage1.Width - 1) || j == (oriimage1.Height - 1))
+                    {
+                        MPenalize[i, j] = 0;
+                    }
+                    else
+                    {
+                        MPenalize[i, j] = 0.5 * oriimage1.GetPixel(i - 1, j - 1).R + oriimage1.GetPixel(i, j - 1).R + 0.5 * oriimage1.GetPixel(i + 1, j - 1).R
+                                      + oriimage1.GetPixel(i - 1, j).R - 6 * oriimage1.GetPixel(i, j).R + oriimage1.GetPixel(i + 1, j).R
+                                      + 0.5 * oriimage1.GetPixel(i - 1, j + 1).R + oriimage1.GetPixel(i, j + 1).R + 0.5 * oriimage1.GetPixel(i + 1, j + 1).R;
+                        if (MPenalize[i, j] > 255)
+                        {
+                            MPenalize[i, j] = 255;
+                        }
+                        if (MPenalize[i, j] < 0)
+                        {
+                            MPenalize[i, j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+        public void plot1()
+        {
+            oriimage0 = new Bitmap(oriimage1, tmpsiz); ;
+
+            double au;
+            Kontur = new double[oriimage1.Width, oriimage1.Height];
+            for (int i = 0; i < oriimage1.Width; i++)
+            {
+                for (int j = 0; j < oriimage1.Height; j++)
+                {
+                    if (i == 0 || j == 0 || i == (oriimage1.Width - 1) || j == (oriimage1.Height - 1))
+                    {
+                        dx[i, j] = 0;
+                        dy[i, j] = 0;
+                    }
+                    else
+                    {
+                        dx[i, j] = (MHeaviside[i + 1, j] - MHeaviside[i, j]) / Math.Sqrt(Math.Pow(MHeaviside[i + 1, j] - MHeaviside[i, j], 2) + Math.Pow((MHeaviside[i, j + 1] - MHeaviside[i, j - 1]) / 2, 2) + 0.0000000001);
+                        dy[i, j] = (MHeaviside[i, j + 1] - MHeaviside[i, j]) / Math.Sqrt(Math.Pow((MHeaviside[i + 1, j] - MHeaviside[i - 1, j]) / 2, 2) + Math.Pow(MHeaviside[i, j + 1] - MHeaviside[i, j], 2) + 0.0000000001);
+                    }
+                    au = Math.Sqrt(Math.Pow(dx[i, j], 2) + Math.Pow(dy[i, j], 2));
+                    if (au != 0)
+                    {
+                        Kontur[i, j] = 1;
+                    }
+                    else
+                    {
+                        Kontur[i, j] = 0;
+                    }
+                }
+            }
+            for (int i = 0; i < oriimage1.Width; i++)
+            {
+                for (int j = 0; j < oriimage1.Height; j++)
+                {
+                    if (Kontur[i, j] == 1)
+                    {
+                        oriimage0.SetPixel(i, j, Color.Red);
+                    }
+                }
+            }
+            panel1.BackgroundImage = oriimage0;
+            panel1.Refresh();
+        }
+    }
+}
